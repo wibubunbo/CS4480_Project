63dc733d9963dd4e7b3978e4ea8187badc132f95
wibubunbo <quangminhha1505@gmail.com>
2024-07-09 23:27:08 +0800

    Update Scala part

--- build.sbt
+++ build.sbt
@@ -5,5 +5,8 @@ scalaVersion := "2.12.15"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.2.0",
   "org.apache.spark" %% "spark-sql" % "3.2.0",
-  "org.apache.spark" %% "spark-mllib" % "3.2.0"
+  "org.apache.spark" %% "spark-mllib" % "3.2.0",
+  "org.apache.lucene" % "lucene-analyzers-common" % "8.8.2",
+  "edu.stanford.nlp" % "stanford-corenlp" % "4.2.0",
+  "edu.stanford.nlp" % "stanford-corenlp" % "4.2.0" classifier "models"
 )
--- src/main/scala/DataCleaning.scala
+++ src/main/scala/DataCleaning.scala
@@ -1,79 +1,117 @@
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
-import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover, Stemmer}
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover}
+import org.tartarus.snowball.ext.PorterStemmer
+import edu.stanford.nlp.simple.Document
 
 object DataCleaning {
   def main(args: Array[String]): Unit = {
-    // Create SparkSession
     val spark = SparkSession.builder()
-      .appName("Amazon Review Sentiment Analysis")
+      .appName("Amazon Review Data Cleaning")
+      .master("local[*]")
       .getOrCreate()
 
-    // Read the JSON files
-    val reviewDF = spark.read.json("path/to/Cell_Phones_and_Accessories.json")
-    val metaDF = spark.read.json("path/to/meta_Cell_Phones_and_Accessories.json")
+    import spark.implicits._
 
-    // Select and clean the columns we need
-    val cleanedDF = reviewDF.select(
-      col("overall"),
-      col("reviewText")
-    ).filter(col("reviewText").isNotNull)
+    // Read the review dataset
+    val df = spark.read.json("Cell_Phones_and_Accessories.json")
 
-    // Define cleaning function
-    def cleanText(text: String): String = {
-      text.replaceAll("<[^>]*>", "") // Remove HTML tags
-        .replaceAll("(https?:\\/\\/www\\.|https?:\\/\\/|www\\.)[\\w\\-]+([\\.\\/\\-\\w]*)*\\S+", "") // Remove URLs
-        .replaceAll("\\S*(#|@|&)\\S*", "") // Remove usernames, hashtags, ampersands
-        .replaceAll("\\d+", "") // Remove numbers
-        .replaceAll("[^a-zA-Z ]", "") // Remove special characters, punctuations
-        .toLowerCase // Convert to lowercase
-    }
+    // Select only the columns we need for sentiment analysis
+    val selectedDF = df.select("overall", "reviewText")
 
-    // Register the UDF
-    val cleanTextUDF = udf(cleanText _)
+    // Clean the text data with three versions
+    val cleanedDF = cleanText(selectedDF)
 
-    // Apply cleaning function
-    val cleanedTextDF = cleanedDF.withColumn("cleaned_reviewText", cleanTextUDF(col("reviewText")))
+    // Filter out reviews with only one word for each version
+    val filteredDF1 = cleanedDF.filter(size(split($"cleaned_reviewText", " ")) > 1)
+    val filteredDF2 = cleanedDF.filter(size(split($"cleaned_reviewText_stemmed", " ")) > 1)
+    val filteredDF3 = cleanedDF.filter(size(split($"cleaned_reviewText_lemmatized", " ")) > 1)
 
-    // Tokenize the cleaned text
-    val tokenizer = new RegexTokenizer()
-      .setInputCol("cleaned_reviewText")
+    // Create balanced datasets
+    val balancedDF1 = createBalancedDataset(filteredDF1, "cleaned_reviewText")
+    val balancedDF2 = createBalancedDataset(filteredDF2, "cleaned_reviewText_stemmed")
+    val balancedDF3 = createBalancedDataset(filteredDF3, "cleaned_reviewText_lemmatized")
+
+    // Split into train and test sets
+    val Array(trainDF1, testDF1) = balancedDF1.randomSplit(Array(0.8, 0.2), seed = 123)
+    val Array(trainDF2, testDF2) = balancedDF2.randomSplit(Array(0.8, 0.2), seed = 123)
+    val Array(trainDF3, testDF3) = balancedDF3.randomSplit(Array(0.8, 0.2), seed = 123)
+
+    // Add index column
+    val trainDFWithIndex1 = addIndexColumn(trainDF1, "cleaned_reviewText")
+    val testDFWithIndex1 = addIndexColumn(testDF1, "cleaned_reviewText")
+    val trainDFWithIndex2 = addIndexColumn(trainDF2, "cleaned_reviewText_stemmed")
+    val testDFWithIndex2 = addIndexColumn(testDF2, "cleaned_reviewText_stemmed")
+    val trainDFWithIndex3 = addIndexColumn(trainDF3, "cleaned_reviewText_lemmatized")
+    val testDFWithIndex3 = addIndexColumn(testDF3, "cleaned_reviewText_lemmatized")
+
+    // Save datasets
+    saveDataset(trainDFWithIndex1, "train1.csv")
+    saveDataset(testDFWithIndex1, "test1.csv")
+    saveDataset(trainDFWithIndex2, "train2.csv")
+    saveDataset(testDFWithIndex2, "test2.csv")
+    saveDataset(trainDFWithIndex3, "train3.csv")
+    saveDataset(testDFWithIndex3, "test3.csv")
+    saveDataset(balancedDF1, "final1.csv")
+    saveDataset(balancedDF2, "final2.csv")
+    saveDataset(balancedDF3, "final3.csv")
+
+    spark.stop()
+  }
+
+  def cleanText(df: DataFrame): DataFrame = {
+    val regexTokenizer = new RegexTokenizer()
+      .setInputCol("reviewText")
       .setOutputCol("words")
-      .setPattern("\\W+")
+      .setPattern("[^a-zA-Z\\s]")
+      .setGaps(false)
 
-    val tokenizedDF = tokenizer.transform(cleanedTextDF)
-
-    // Remove stop words
-    val remover = new StopWordsRemover()
+    val stopWordsRemover = new StopWordsRemover()
       .setInputCol("words")
       .setOutputCol("filtered_words")
 
-    val removedStopWordsDF = remover.transform(tokenizedDF)
+    val stemmer = new PorterStemmer()
+    val stemUDF = udf((words: Seq[String]) => words.map(word => {
+      stemmer.setCurrent(word)
+      stemmer.stem()
+      stemmer.getCurrent
+    }))
 
-    // Apply stemming
-    val stemmer = new Stemmer()
-      .setInputCol("filtered_words")
-      .setOutputCol("stemmed_words")
+    val lemmatizeUDF = udf((text: String) => {
+      val doc = new Document(text)
+      doc.sentences().asScala.flatMap(_.lemmas().asScala).mkString(" ")
+    })
 
-    val stemmedDF = stemmer.transform(removedStopWordsDF)
+    df.withColumn("reviewText", lower($"reviewText"))
+      .transform(regexTokenizer.transform(_))
+      .transform(stopWordsRemover.transform(_))
+      .withColumn("cleaned_reviewText", concat_ws(" ", $"filtered_words"))
+      .withColumn("cleaned_reviewText_stemmed", concat_ws(" ", stemUDF($"filtered_words")))
+      .withColumn("cleaned_reviewText_lemmatized", lemmatizeUDF($"reviewText"))
+      .select("overall", "cleaned_reviewText", "cleaned_reviewText_stemmed", "cleaned_reviewText_lemmatized")
+  }
 
-    // Join words back into a single string
-    val finalDF = stemmedDF.withColumn("final_text", concat_ws(" ", col("stemmed_words")))
+  def createBalancedDataset(df: DataFrame, textColumn: String): DataFrame = {
+    val negativeReviews = df.filter($"overall".isin(1.0, 2.0))
+    val positiveReviews = df.filter($"overall" === 5.0)
+    val sampleSize = negativeReviews.count()
 
-    // Filter out reviews with only one word
-    val multiWordDF = finalDF.filter(size(col("stemmed_words")) > 1)
+    val sampledPositiveReviews = positiveReviews.sample(withReplacement = false, fraction = sampleSize.toDouble / positiveReviews.count(), seed = 123)
+    val balancedDF = negativeReviews.union(sampledPositiveReviews)
 
-    // Prepare data for sentiment analysis
-    val sentimentDF = multiWordDF.withColumn("sentiment", when(col("overall") === 5.0, 1).otherwise(0))
+    balancedDF.select("overall", textColumn).orderBy(rand())
+  }
 
-    // Split into train and test sets
-    val Array(trainDF, testDF) = sentimentDF.randomSplit(Array(0.8, 0.2), seed = 42)
+  def addIndexColumn(df: DataFrame, textColumn: String): DataFrame = {
+    df.withColumn("index", monotonically_increasing_id() + 1)
+      .select("index", "overall", textColumn)
+  }
 
-    // Save train and test sets
-    trainDF.write.csv("path/to/repo/train_data.csv")
-    testDF.write.csv("/path/to/repo/test_data.csv")
-
-    // Stop SparkSession
-    spark.stop()
+  def saveDataset(df: DataFrame, filename: String): Unit = {
+    df.coalesce(1)
+      .write
+      .option("header", "false")
+      .option("delimiter", ",")
+      .csv(filename)
   }
 }
